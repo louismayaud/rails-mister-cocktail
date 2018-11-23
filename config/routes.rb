dce59993904396b7eec42d93017f6f92b77d76e5
Louis Mayaud <louis.myd@gmail.com>
2018-11-23 11:11:50 +0100

    rails mister cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
+  resources :cocktails, only: [:destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
